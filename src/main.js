73191b48d5c3cc177e984db0ef7e28bd5b292cc9
bfloriano <b.floriano@outlook.com>
2019-09-13 01:44:10 -0300

    exibição dos dados na tela em forma de cards

---
+++ src/main.js
@@ -0,0 +1,23 @@
+
+const POKEMONS = POKEMON.pokemon;
+
+const img = document.getElementById("result");
+POKEMONS.map(poke => {
+    let card = `<div class="card"><div class="card-inner">
+    <div class="card-front"><img src="${poke.img}"><p class="nome">${poke.name}</p></div>
+    <div class="card-back">Teste</div>
+    </div></div>`;
+    img.innerHTML += `${card}`;
+    // img.innerHTML += `<img src="${poke.img}">${poke.name}</br>`
+
+
+}
+)
+
+
+/*
+for (let poke of POKEMONS){
+    const img = document.getElementById("result");
+    img.innerHTML += `<img src="${poke.img}">${poke.name}</br>`    
+}
+*/
