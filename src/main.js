3f2e422bdb344b7b51a3f972b535ce2c5409ede6
bfloriano <b.floriano@outlook.com>
2019-09-24 02:07:57 -0300

    funções filtro e ordenação otimizadas, e testes unitários

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 const POKEMONS = POKEMON.pokemon;
-const cartao = document.getElementById("cartao");
+let cartao = document.getElementById("cartao");
 let botaoFiltro = document.getElementById("botao-filtro");
 let botaoFraqueza = document.getElementById("botao-fraqueza");
 let botaoOrdem = document.getElementById("botao-ordem");
 let botaoRaridade = document.getElementById("botao-raridade");
 
-//executar a função ao abrir a pagina para listar as opções de filtro e os cards
+//executar a função ao abrir a pagina para listar as opções dos botões e os cards
 onload = () => {
   exibeCartao(POKEMONS);
   listaTipos(POKEMONS);
   listaFraqueza(POKEMONS);
-    
 };
 
 //exibição do card
@@ -31,12 +30,7 @@ function exibeCartao(arr) {
   cartao.innerHTML = cardzinho;
 }
    
-// passo 1 = pegar cada tipo e fazer uma lista em forma de array / criar menu com a array criada anteriormente
-// passo 2 = função (data.js)
-// passo 3 = trem que junta TUDO e exibe certo o que eu quero
-
-// passo 1
-//FILTRO-TIPO
+//BOTÃO FILTRO-TIPO
 function listaTipos(POKEMONS) {
   const filterTypes = []; 
   POKEMONS.map(poke => poke.type.map(type => {
@@ -48,11 +42,11 @@ function listaTipos(POKEMONS) {
   }));
 
   botaoFiltro.innerHTML = "";
-  botaoFiltro.innerHTML = `<option value="none">Tipo</option>`;
+  botaoFiltro.innerHTML = "<option value=\"none\">Tipo</option>";
   botaoFiltro.innerHTML += filterTypes.map(type => `<option value="${type}">${type}</option>`).join("");
 }
 
-//FILTRO-FRAQUEZA
+//BOTÃO FILTRO-FRAQUEZA
 function listaFraqueza(POKEMONS) {
   const filterWeak = []; 
   POKEMONS.map(poke => poke.weaknesses.map(weaknesses => {
@@ -64,43 +58,51 @@ function listaFraqueza(POKEMONS) {
   }));
 
   botaoFraqueza.innerHTML = "";
-  botaoFraqueza.innerHTML = `<option value="none">Fraqueza</option>`;
+  botaoFraqueza.innerHTML = "<option value=\"none\">Fraqueza</option>";
   botaoFraqueza.innerHTML += filterWeak.map(weaknesses => `<option value="${weaknesses}">${weaknesses}</option>`).join("");
 }
 
-//passo 3
-botaoFiltro.addEventListener("change", filtro);
-botaoFraqueza.addEventListener("change", filtro);
+//BOTÕES ORDENAR
+botaoOrdem.innerHTML = "<option value=\"none\">Nome</option> <option value=\"A - Z\">A - Z</option> <option value=\"Z - A\"> Z - A</option>";
+botaoRaridade.innerHTML = "<option value=\"none\">Raridade</option> <option value=\"maisRaros\">Raros</option> <option value=\"menosRaros\">Comuns</option>";
 
+//Evento dos botões
+botaoFiltro.addEventListener("change", filtro);
+botaoFraqueza.addEventListener("change", fraqueza);
+botaoOrdem.addEventListener("change", ordem);
+botaoRaridade.addEventListener("change", raridade);
+
+//FUNÇÕES
 function filtro() {
-  if (botaoFiltro.value == "none" && botaoFraqueza.value == "none") {
-    exibeCartao(POKEMONS);
-  } else if (botaoFiltro.value == "none" && botaoFraqueza.value != "none") {
-    exibeCartao(app.filtrarFraqueza(POKEMONS, botaoFraqueza.value));
-  } else if (botaoFiltro.value != "none" && botaoFraqueza.value == "none") {
-    exibeCartao(app.filtrarTipo(POKEMONS, botaoFiltro.value));
-  } else if ((botaoFiltro.value != "none" && botaoFraqueza.value != "none")) {
-    alert("não é possivel utilizar dois filtros ao mesmo tempo, por favor, selecione apenas uma opção");
-    botaoFiltro.value = "none";
-    botaoFraqueza.value = "none";
+  exibeCartao(app.filtrar(POKEMONS, botaoFiltro.value, "type"));
+  if (botaoFiltro.value == "none") {
     exibeCartao(POKEMONS);
   }
-}  
-    
-//ordenação NOME
-
-botaoOrdem.innerHTML = `<option>Nome</option> <option value="A - Z">A - Z</option> <option value="Z - A"> Z - A</option>`;
-botaoOrdem.addEventListener("change", () => listaOrdenada(POKEMONS, botaoOrdem.value));
-
-function listaOrdenada(arr, condition) {
-  exibeCartao(app.ordemAlfabetica(arr, condition));
+  botaoFraqueza.value = "none";
+  botaoOrdem.value = "none";
+  botaoRaridade.value = "none";
 }
 
-//ordenação RARIDADE
+function fraqueza() {
+  exibeCartao(app.filtrar(POKEMONS, botaoFraqueza.value, "weaknesses"));
+  if (botaoFraqueza.value == "none") {
+    exibeCartao(POKEMONS);
+  }
+  botaoFiltro.value = "none";
+  botaoOrdem.value = "none";
+  botaoRaridade.value = "none";
+} 
 
-botaoRaridade.innerHTML = `<option>Raridade</option> <option value="maisRaros">Pokemons mais raros</option> <option value="menosRaros">Pokemons menos raros</option>`;
-botaoRaridade.addEventListener("change", () => listaRaridade(POKEMONS, botaoRaridade.value));
+function ordem () {
+  exibeCartao(app.ordenar(POKEMONS, botaoOrdem.value, "name"));
+  botaoFiltro.value = "none";
+  botaoFraqueza.value = "none";
+  botaoRaridade.value = "none";
+} 
 
-function listaRaridade(arr, condition) {
-  exibeCartao(app.ordemRaridade(arr, condition));
+function raridade () {
+  exibeCartao(app.ordenar(POKEMONS, botaoRaridade.value, "spawn_chance"));
+  botaoFiltro.value = "none";
+  botaoFraqueza.value = "none";
+  botaoOrdem.value = "none";
 }
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,149 @@
-require('../src/data.js');
+require("../src/data.js");
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe("função filtro", () => {
+  it("is a function", () => {
+    expect(typeof app.filtrar).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it("returns `pokemons filtrados por categoria tipo`", () => {
+    expect(app.filtrar([
+      {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+        "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+        "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+        "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}, 
+      {"id": 4, "num": "004", "name": "Charmander", "img": "http://www.serebii.net/pokemongo/pokemon/004.png", "type": ["Fire"], 
+        "height": "0.61 m", "weight": "8.5 kg", "candy": "Charmander Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.253, 
+        "avg_spawns": 25.3, "spawn_time": "08:45", "multipliers": [1.65], "weaknesses": ["Water", "Ground", "Rock"], 
+        "next_evolution": [{"num": "005", "name": "Charmeleon"}, {"num": "006", "name": "Charizard"}]},
+      {"id": 9, "num": "009", "name": "Blastoise", "img": "http://www.serebii.net/pokemongo/pokemon/009.png", "type": ["Water"],
+        "height": "1.60 m", "weight": "85.5 kg", "candy": "Squirtle Candy", "egg": "Not in Eggs", "spawn_chance": 0.0067,
+        "avg_spawns": 0.67, "spawn_time": "00:06", "multipliers": null, "weaknesses": ["Electric", "Grass"],
+        "prev_evolution": [{"num": "007", "name": "Squirtle"}, {"num": "008", "name": "Wartortle"}]}, 
+      {"id": 72, "num": "072", "name": "Tentacool", "img": "http://www.serebii.net/pokemongo/pokemon/072.png", "type": ["Water", "Poison"],
+        "height": "0.89 m", "weight": "45.5 kg", "candy": "Tentacool Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 0.81,
+        "avg_spawns": 81, "spawn_time": "03:20", "multipliers": [2.52], "weaknesses": ["Electric", "Ground", "Psychic"],
+        "next_evolution": [{"num": "073", "name": "Tentacruel"}]}
+    ], "Water", "type" )).toEqual([
+      {"id": 9, "num": "009", "name": "Blastoise", "img": "http://www.serebii.net/pokemongo/pokemon/009.png", "type": ["Water"],
+        "height": "1.60 m", "weight": "85.5 kg", "candy": "Squirtle Candy", "egg": "Not in Eggs", "spawn_chance": 0.0067,
+        "avg_spawns": 0.67, "spawn_time": "00:06", "multipliers": null, "weaknesses": ["Electric", "Grass"],
+        "prev_evolution": [{"num": "007", "name": "Squirtle"}, {"num": "008", "name": "Wartortle"}]}, 
+      {"id": 72, "num": "072", "name": "Tentacool", "img": "http://www.serebii.net/pokemongo/pokemon/072.png", "type": ["Water", "Poison"],
+        "height": "0.89 m", "weight": "45.5 kg", "candy": "Tentacool Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 0.81,
+        "avg_spawns": 81, "spawn_time": "03:20", "multipliers": [2.52], "weaknesses": ["Electric", "Ground", "Psychic"],
+        "next_evolution": [{"num": "073", "name": "Tentacruel"}]} 
+    ]);
   });
+});
+
+it("returns `pokemons filtrados por categoria fraqueza`", () => {
+  expect(app.filtrar([
+    {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+      "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+      "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+      "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}, 
+    {"id": 4, "num": "004", "name": "Charmander", "img": "http://www.serebii.net/pokemongo/pokemon/004.png", "type": ["Fire"], 
+      "height": "0.61 m", "weight": "8.5 kg", "candy": "Charmander Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.253, 
+      "avg_spawns": 25.3, "spawn_time": "08:45", "multipliers": [1.65], "weaknesses": ["Water", "Ground", "Rock"], 
+      "next_evolution": [{"num": "005", "name": "Charmeleon"}, {"num": "006", "name": "Charizard"}]},
+    {"id": 9, "num": "009", "name": "Blastoise", "img": "http://www.serebii.net/pokemongo/pokemon/009.png", "type": ["Water"],
+      "height": "1.60 m", "weight": "85.5 kg", "candy": "Squirtle Candy", "egg": "Not in Eggs", "spawn_chance": 0.0067,
+      "avg_spawns": 0.67, "spawn_time": "00:06", "multipliers": null, "weaknesses": ["Electric", "Grass"],
+      "prev_evolution": [{"num": "007", "name": "Squirtle"}, {"num": "008", "name": "Wartortle"}]}, 
+    {"id": 72, "num": "072", "name": "Tentacool", "img": "http://www.serebii.net/pokemongo/pokemon/072.png", "type": ["Water", "Poison"],
+      "height": "0.89 m", "weight": "45.5 kg", "candy": "Tentacool Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 0.81,
+      "avg_spawns": 81, "spawn_time": "03:20", "multipliers": [2.52], "weaknesses": ["Electric", "Ground", "Psychic"],
+      "next_evolution": [{"num": "073", "name": "Tentacruel"}]}
+  ], "Psychic", "weaknesses" )).toEqual([
+    {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+      "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+      "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+      "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}, 
+    {"id": 72, "num": "072", "name": "Tentacool", "img": "http://www.serebii.net/pokemongo/pokemon/072.png", "type": ["Water", "Poison"],
+      "height": "0.89 m", "weight": "45.5 kg", "candy": "Tentacool Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 0.81,
+      "avg_spawns": 81, "spawn_time": "03:20", "multipliers": [2.52], "weaknesses": ["Electric", "Ground", "Psychic"],
+      "next_evolution": [{"num": "073", "name": "Tentacruel"}]} 
+  ]);
+});
+
+describe("função ordem", () => {
+  it("is a function", () => {
+    expect(typeof app.ordenar).toBe("function");
+  });
+
+  it("returns `pokemons ordenados por nome`", () => {
+    expect(app.ordenar([
+      {"id": 41, "num": "041", "name": "Zubat", "img": "http://www.serebii.net/pokemongo/pokemon/041.png", "type": ["Poison", "Flying"],
+        "height": "0.79 m", "weight": "7.5 kg", "candy": "Zubat Candy", "candy_count": 50, "egg": "2 km", "spawn_chance": 6.52,
+        "avg_spawns": 652, "spawn_time": "12:28", "multipliers": [2.6, 3.67], "weaknesses": ["Electric", "Ice", "Psychic", "Rock"],
+        "next_evolution": [{"num": "042", "name": "Golbat"}]}, 
+      {"id": 54, "num": "054", "name": "Psyduck", "img": "http://www.serebii.net/pokemongo/pokemon/054.png", "type": ["Water"],
+        "height": "0.79 m", "weight": "19.6 kg", "candy": "Psyduck Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 2.54,
+        "avg_spawns": 254, "spawn_time": "03:41", "multipliers": [2.27], "weaknesses": ["Electric", "Grass"],
+        "next_evolution": [{"num": "055", "name": "Golduck"}]}, 
+      {"id": 96, "num": "096", "name": "Drowzee", "img": "http://www.serebii.net/pokemongo/pokemon/096.png", "type": ["Psychic"],
+        "height": "0.99 m", "weight": "32.4 kg", "candy": "Drowzee Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 3.21,
+        "avg_spawns": 321, "spawn_time": "01:51", "multipliers": [2.08, 2.09], "weaknesses": ["Bug", "Ghost", "Dark"],
+        "next_evolution": [{"num": "097", "name": "Hypno"}]}, 
+      {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+        "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+        "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+        "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}
+    ], "A - Z", "name" )).toEqual([
+      {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+        "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+        "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+        "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}, 
+      {"id": 96, "num": "096", "name": "Drowzee", "img": "http://www.serebii.net/pokemongo/pokemon/096.png", "type": ["Psychic"],
+        "height": "0.99 m", "weight": "32.4 kg", "candy": "Drowzee Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 3.21,
+        "avg_spawns": 321, "spawn_time": "01:51", "multipliers": [2.08, 2.09], "weaknesses": ["Bug", "Ghost", "Dark"],
+        "next_evolution": [{"num": "097", "name": "Hypno"}]}, 
+      {"id": 54, "num": "054", "name": "Psyduck", "img": "http://www.serebii.net/pokemongo/pokemon/054.png", "type": ["Water"],
+        "height": "0.79 m", "weight": "19.6 kg", "candy": "Psyduck Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 2.54,
+        "avg_spawns": 254, "spawn_time": "03:41", "multipliers": [2.27], "weaknesses": ["Electric", "Grass"],
+        "next_evolution": [{"num": "055", "name": "Golduck"}]}, 
+      {"id": 41, "num": "041", "name": "Zubat", "img": "http://www.serebii.net/pokemongo/pokemon/041.png", "type": ["Poison", "Flying"],
+        "height": "0.79 m", "weight": "7.5 kg", "candy": "Zubat Candy", "candy_count": 50, "egg": "2 km", "spawn_chance": 6.52,
+        "avg_spawns": 652, "spawn_time": "12:28", "multipliers": [2.6, 3.67], "weaknesses": ["Electric", "Ice", "Psychic", "Rock"],
+        "next_evolution": [{"num": "042", "name": "Golbat"}]}
+    ]);
+  });
+});
+
+it("returns `pokemons ordenados por raridade`", () => {
+  expect(app.ordenar([
+    {"id": 41, "num": "041", "name": "Zubat", "img": "http://www.serebii.net/pokemongo/pokemon/041.png", "type": ["Poison", "Flying"],
+      "height": "0.79 m", "weight": "7.5 kg", "candy": "Zubat Candy", "candy_count": 50, "egg": "2 km", "spawn_chance": 6.52,
+      "avg_spawns": 652, "spawn_time": "12:28", "multipliers": [2.6, 3.67], "weaknesses": ["Electric", "Ice", "Psychic", "Rock"],
+      "next_evolution": [{"num": "042", "name": "Golbat"}]}, 
+    {"id": 54, "num": "054", "name": "Psyduck", "img": "http://www.serebii.net/pokemongo/pokemon/054.png", "type": ["Water"],
+      "height": "0.79 m", "weight": "19.6 kg", "candy": "Psyduck Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 2.54,
+      "avg_spawns": 254, "spawn_time": "03:41", "multipliers": [2.27], "weaknesses": ["Electric", "Grass"],
+      "next_evolution": [{"num": "055", "name": "Golduck"}]}, 
+    {"id": 96, "num": "096", "name": "Drowzee", "img": "http://www.serebii.net/pokemongo/pokemon/096.png", "type": ["Psychic"],
+      "height": "0.99 m", "weight": "32.4 kg", "candy": "Drowzee Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 3.21,
+      "avg_spawns": 321, "spawn_time": "01:51", "multipliers": [2.08, 2.09], "weaknesses": ["Bug", "Ghost", "Dark"],
+      "next_evolution": [{"num": "097", "name": "Hypno"}]}, 
+    {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+      "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+      "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+      "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}
+  ], "menosRaros", "spawn_chance" )).toEqual([
+    {"id": 41, "num": "041", "name": "Zubat", "img": "http://www.serebii.net/pokemongo/pokemon/041.png", "type": ["Poison", "Flying"],
+      "height": "0.79 m", "weight": "7.5 kg", "candy": "Zubat Candy", "candy_count": 50, "egg": "2 km", "spawn_chance": 6.52,
+      "avg_spawns": 652, "spawn_time": "12:28", "multipliers": [2.6, 3.67], "weaknesses": ["Electric", "Ice", "Psychic", "Rock"],
+      "next_evolution": [{"num": "042", "name": "Golbat"}]},
+    {"id": 96, "num": "096", "name": "Drowzee", "img": "http://www.serebii.net/pokemongo/pokemon/096.png", "type": ["Psychic"],
+      "height": "0.99 m", "weight": "32.4 kg", "candy": "Drowzee Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 3.21,
+      "avg_spawns": 321, "spawn_time": "01:51", "multipliers": [2.08, 2.09], "weaknesses": ["Bug", "Ghost", "Dark"],
+      "next_evolution": [{"num": "097", "name": "Hypno"}]}, 
+    {"id": 54, "num": "054", "name": "Psyduck", "img": "http://www.serebii.net/pokemongo/pokemon/054.png", "type": ["Water"],
+      "height": "0.79 m", "weight": "19.6 kg", "candy": "Psyduck Candy", "candy_count": 50, "egg": "5 km", "spawn_chance": 2.54,
+      "avg_spawns": 254, "spawn_time": "03:41", "multipliers": [2.27], "weaknesses": ["Electric", "Grass"],
+      "next_evolution": [{"num": "055", "name": "Golduck"}]}, 
+    {"id": 1, "num": "001", "name": "Bulbasaur", "img": "http://www.serebii.net/pokemongo/pokemon/001.png", "type": ["Grass", "Poison"],
+      "height": "0.71 m", "weight": "6.9 kg", "candy": "Bulbasaur Candy", "candy_count": 25, "egg": "2 km", "spawn_chance": 0.69, 
+      "avg_spawns": 69, "spawn_time": "20:00", "multipliers": [1.58], "weaknesses": ["Fire", "Ice", "Flying", "Psychic"], 
+      "next_evolution": [{"num": "002", "name": "Ivysaur"}, {"num": "003", "name": "Venusaur"}]}
+  ]);
 });
