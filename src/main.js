5c5c93a1148abf638a494f1ae226aed3d78b3712
campostalita <campos.talita@hotmail.com>
2020-01-28 11:23:07 -0300

    arrumando nomenclatura de funcoes

--- src/main.js
+++ src/main.js
@@ -1,23 +1,40 @@
+/* const getCards = () => {
+  fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")
+    .then(response => response.json())
+    .then(data => {
+      //fetch(data.results[index].url)
+      data.results.map(pokemon => {
+        fetch(pokemon.url)
+          .then(response => response.json())
+          .then(data2 => {
+            let picturePoke = data2.sprites.front_default;
+            let namePoke = data2.name;
+            let getPokeType = data2.types.map(type => type.type.name);
+            pokecards({ name: namePoke, img: picturePoke, type: getPokeType });
+          });
+      });
+    }); */
+
 const POKEMONS = POKEMON.pokemon;
 let cartao = document.getElementById("cartao");
-let botaoFiltro = document.getElementById("botao-filtro");
-let botaoFraqueza = document.getElementById("botao-fraqueza");
-let botaoOrdem = document.getElementById("botao-ordem");
-let botaoRaridade = document.getElementById("botao-raridade");
+let filterButton = document.getElementById("filter-button");
+let weaknessButton = document.getElementById("weakness-button");
+let sortButton = document.getElementById("sort-button");
+let rarityButton = document.getElementById("rarity-button");
 let medias = document.getElementById("medias");
-let ver = document.getElementById("ver");
-let vertodos = document.getElementById("vertodos");
-let busca = document.getElementById("busca");
+let seeButton = document.getElementById("see-button");
+let seeAllButton = document.getElementById("see-all-button");
+let searchPokemon = document.getElementById("search-pokemon");
 
 //executar a função ao abrir a pagina para listar as opções dos botões e os cards
 onload = () => {
-  exibeCartao(POKEMONS);
-  listaTipos(POKEMONS);
-  listaFraqueza(POKEMONS);  
+  cardDisplay(POKEMONS);
+  typeList(POKEMONS);
+  weaknessList(POKEMONS);  
 };
 
 //exibição do card
-function exibeCartao(arr) {
+function cardDisplay(arr) {
   let cardzinho ="";
   arr.forEach(poke => {
     cardzinho += `
@@ -35,7 +52,7 @@ function exibeCartao(arr) {
 }
    
 //BOTÃO FILTRO-TIPO
-function listaTipos(POKEMONS) {
+function typeList(POKEMONS) {
   const filterTypes = []; 
   POKEMONS.map(poke => poke.type.map(type => {
     if (!filterTypes.includes(type)) {
@@ -45,13 +62,13 @@ function listaTipos(POKEMONS) {
     }   
   }));
 
-  botaoFiltro.innerHTML = "";
-  botaoFiltro.innerHTML = "<option value=\"none\">Tipo</option>";
-  botaoFiltro.innerHTML += filterTypes.map(type => `<option value="${type}">${type}</option>`).join("");
+  filterButton.innerHTML = "";
+  filterButton.innerHTML = "<option value=\"none\">Tipo</option>";
+  filterButton.innerHTML += filterTypes.map(type => `<option value="${type}">${type}</option>`).join("");
 }
 
 //BOTÃO FILTRO-FRAQUEZA
-function listaFraqueza(POKEMONS) {
+function weaknessList(POKEMONS) {
   const filterWeak = []; 
   POKEMONS.map(poke => poke.weaknesses.map(weaknesses => {
     if (!filterWeak.includes(weaknesses)) {
@@ -61,95 +78,95 @@ function listaFraqueza(POKEMONS) {
     }   
   }));
 
-  botaoFraqueza.innerHTML = "";
-  botaoFraqueza.innerHTML = "<option value=\"none\">Fraqueza</option>";
-  botaoFraqueza.innerHTML += filterWeak.map(weaknesses => `<option value="${weaknesses}">${weaknesses}</option>`).join("");
+  weaknessButton.innerHTML = "";
+  weaknessButton.innerHTML = "<option value=\"none\">Fraqueza</option>";
+  weaknessButton.innerHTML += filterWeak.map(weaknesses => `<option value="${weaknesses}">${weaknesses}</option>`).join("");
 }
 
 //BOTÕES ORDENAR
-botaoOrdem.innerHTML = "<option value=\"none\">Nome</option> <option value=\"A - Z\">A - Z</option> <option value=\"Z - A\"> Z - A</option>";
-botaoRaridade.innerHTML = "<option value=\"none\">Raridade</option> <option value=\"maisRaros\">Raros</option> <option value=\"menosRaros\">Comuns</option>";
+sortButton.innerHTML = "<option value=\"none\">Nome</option> <option value=\"A - Z\">A - Z</option> <option value=\"Z - A\"> Z - A</option>";
+rarityButton.innerHTML = "<option value=\"none\">Raridade</option> <option value=\"maisRaros\">Raros</option> <option value=\"menosRaros\">Comuns</option>";
 
 //Evento dos botões
-botaoFiltro.addEventListener("change", filtro);
-botaoFraqueza.addEventListener("change", fraqueza);
-botaoOrdem.addEventListener("change", ordem);
-botaoRaridade.addEventListener("change", raridade);
+filterButton.addEventListener("change", filtro);
+weaknessButton.addEventListener("change", fraqueza);
+sortButton.addEventListener("change", ordem);
+rarityButton.addEventListener("change", raridade);
 
 //FUNÇÕES
 function filtro() {
-  exibeCartao(app.filtrar(POKEMONS, botaoFiltro.value, "type"));
-  if (botaoFiltro.value == "none") {
-    exibeCartao(POKEMONS);
+  cardDisplay(app.filtrar(POKEMONS, filterButton.value, "type"));
+  if (filterButton.value == "none") {
+    cardDisplay(POKEMONS);
   }
-  botaoFraqueza.value = "none";
-  botaoOrdem.value = "none";
-  botaoRaridade.value = "none";
+  weaknessButton.value = "none";
+  sortButton.value = "none";
+  rarityButton.value = "none";
 }
 
 function fraqueza() {
-  exibeCartao(app.filtrar(POKEMONS, botaoFraqueza.value, "weaknesses"));
-  if (botaoFraqueza.value == "none") {
-    exibeCartao(POKEMONS);
+  cardDisplay(app.filtrar(POKEMONS, weaknessButton.value, "weaknesses"));
+  if (weaknessButton.value == "none") {
+    cardDisplay(POKEMONS);
   }
-  botaoFiltro.value = "none";
-  botaoOrdem.value = "none";
-  botaoRaridade.value = "none";
+  filterButton.value = "none";
+  sortButton.value = "none";
+  rarityButton.value = "none";
 } 
 
 function ordem () {
-  exibeCartao(app.ordenar(POKEMONS, botaoOrdem.value, "name"));
-  botaoFiltro.value = "none";
-  botaoFraqueza.value = "none";
-  botaoRaridade.value = "none";
+  cardDisplay(app.ordenar(POKEMONS, sortButton.value, "name"));
+  filterButton.value = "none";
+  weaknessButton.value = "none";
+  rarityButton.value = "none";
 } 
 
 function raridade () {
-  exibeCartao(app.ordenar(POKEMONS, botaoRaridade.value, "spawn_chance"));
-  botaoFiltro.value = "none";
-  botaoFraqueza.value = "none";
-  botaoOrdem.value = "none";
+  cardDisplay(app.ordenar(POKEMONS, rarityButton.value, "spawn_chance"));
+  filterButton.value = "none";
+  weaknessButton.value = "none";
+  sortButton.value = "none";
 }
 
 //CALCULOS CURIOSIDADES
-const arrAltura = (POKEMONS.map(poke => poke.height)).map((i) => Number(i.replace(/[^0-9.,]+/g, "")));
-const arrPeso = (POKEMONS.map(poke => poke.weight)).map((i) => Number(i.replace(/[^0-9.,]+/g, "")));
+const arrHeight = (POKEMONS.map(poke => poke.height)).map((i) => Number(i.replace(/[^0-9.,]+/g, "")));
+const arrWeight = (POKEMONS.map(poke => poke.weight)).map((i) => Number(i.replace(/[^0-9.,]+/g, "")));
 
 //maiores e menores
 const nome = POKEMONS.map(poke => poke.name);
 
-const menorAltura = (Math.min.apply(Math, arrAltura));
-const menorPoke = nome[(arrAltura.indexOf(menorAltura))];
-const maiorAltura = (Math.max.apply(Math, arrAltura));
-const maiorPoke = nome[(arrAltura.indexOf(maiorAltura))];
+const lowerHeight = (Math.min.apply(Math, arrHeight));
+const smallestPokemon = nome[(arrHeight.indexOf(lowerHeight))];
+const higherPokemon = (Math.max.apply(Math, arrHeight));
+const biggestPokemon = nome[(arrHeight.indexOf(higherPokemon))];
 
-const menorPeso = (Math.min.apply(Math, arrPeso));
-const magrinhoPoke = nome[(arrPeso.indexOf(menorPeso))];
-const maiorPeso = (Math.max.apply(Math, arrPeso));
-const gordinhoPoke = nome[(arrPeso.indexOf(maiorPeso))];
+const lowerWeight = (Math.min.apply(Math, arrWeight));
+const skinnyPokemon = nome[(arrWeight.indexOf(lowerWeight))];
+const greaterWeight = (Math.max.apply(Math, arrWeight));
+const fatPokemon = nome[(arrWeight.indexOf(greaterWeight))];
 
 //tela de curiosidades
 medias.innerHTML = `A <strong>média de Altura</strong> de todos os pokemons é: ${app.media(POKEMONS.map(poke => poke.height)).toFixed(2)} metros.</br>
-Sendo o menor deles é o ${menorPoke}, medindo apenas ${menorAltura} metros, e o maior é o ${maiorPoke}, medindo ${maiorAltura} metros.</br>`;
+Sendo o menor deles é o ${smallestPokemon}, medindo apenas ${lowerHeight} metros, e o maior é o ${biggestPokemon}, medindo ${higherPokemon} metros.</br>`;
 medias.innerHTML += `</br>A <strong>média do Peso</strong> de todos os pokemons é: ${app.media(POKEMONS.map(poke => poke.weight)).toFixed(2)} kg.</br>
-O mais leve deles é o ${magrinhoPoke}, com apenas ${menorPeso} kg, enquanto o mais pesado é o ${gordinhoPoke}, pesando exatos ${maiorPeso} kg.`;
+O mais leve deles é o ${skinnyPokemon}, com apenas ${lowerWeight} kg, enquanto o mais pesado é o ${fatPokemon}, pesando exatos ${greaterWeight} kg.`;
 
-let pokesExtremos = [POKEMONS[(arrAltura.indexOf(menorAltura))], POKEMONS[(arrAltura.indexOf(maiorAltura))], POKEMONS[(arrPeso.indexOf(menorPeso))], POKEMONS[(arrPeso.indexOf(maiorPeso))]];
-const extremos = () => (exibeCartao(pokesExtremos));
+let pokesExtremos = [POKEMONS[(arrHeight.indexOf(lowerHeight))], POKEMONS[(arrHeight.indexOf(higherPokemon))], POKEMONS[(arrWeight.indexOf(lowerWeight))], POKEMONS[(arrWeight.indexOf(greaterWeight))]];
+const extremos = () => (cardDisplay(pokesExtremos));
 const todos = () => {
-  (exibeCartao(POKEMONS));
-  botaoFiltro.value = "none"; 
-  botaoFraqueza.value = "none";
+  (cardDisplay(POKEMONS));
+  filterButton.value = "none"; 
+  weaknessButton.value = "none";
 };
 
-ver.addEventListener("click", extremos);
-vertodos.addEventListener("click", todos);
+seeButton.addEventListener("click", extremos);
+seeAllButton.addEventListener("click", todos);
 
 // Barra de busca
-busca.addEventListener("keyup", () => {
-  exibeCartao(app.filtrar(POKEMONS, busca.value, "name"));
-  botaoFiltro.value = "none";
-  botaoFraqueza.value = "none";
-  botaoOrdem.value = "none";
-  botaoRaridade.value = "none";
+searchPokemon.addEventListener("keyup", () => {
+  cardDisplay(app.filtrar(POKEMONS, searchPokemon.value, "name"));
+  filterButton.value = "none";
+  weaknessButton.value = "none";
+  sortButton.value = "none";
+  rarityButton.value = "none";
 });
